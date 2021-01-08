c261e3e6ce7a9686cf02772ad8d75f7ad5a60ad1
ethan-cheong <>
2021-01-08 19:03:10 +0800

    Add app framework

--- app.py
+++ app.py
@@ -1,18 +1,21 @@
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
-from dash.dependencies import Input, Output
+from dash.dependencies import Input, Output, State
 import plotly.express as px
 import pandas as pd
 
 #Plan:
+
+# Put our big big map on the right hand side.
+# Slowly add in functionality for all buttons.
+
 # Two visualization types, heatmap and scatter.
 # Put these in the header: Select vizualisation type.
 # Will affect menus that pop up,
 
-# For heatmap, options will be year and month. Year and month will both be
-# Adjustable ranges.
-# Radio items that suggest which crime you want.
+# For heatmap, options will be by year. Year should be a range.
+# multiple choice dropdown menu to choose crimes. Also have checkbox to pick all.
 # Optional Postal code entry that will show where you want. Otherwise,
 # Big map of the UK.
 # Heatmap will group by some district - find a way to do this. maybe upload a
@@ -30,12 +33,15 @@ import pandas as pd
 # metrics.
 
 # Once you've done that, tidy up everything. sort out transparency, colours.
+# Finally, OPTIMIZE! make fast.
 
 df_street = pd.read_csv("street_data.csv")
 # Change the writing format for concatDatasets so it writes a usable date!
 
 token = "pk.eyJ1IjoiZXRoYW5jaGVvbmciLCJhIjoiY2tqbWRtdmNnMDN2dTJ3cGVyOHdpaDJuOSJ9.v7rcDmVITTKevrcT5HCXKA"
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
+
+chart_options = ['chart a', 'chart b']
 
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 
@@ -46,65 +52,152 @@ app.layout = html.Div([
                id='description')
     ], id='header'
     ),
+
     html.Div([
 
         html.Div([
 
-            html.P(
-                'Drag the slider to change the year:',
-                id='year-slider-text'),
-
-            dcc.Slider(
-                id='year-slider',
-                min=df_street['Year'].min(),
-                max=df_street['Year'].max(),
-                value=df_street['Year'].min(),
-                marks={str(year): str(year) for year in df_street['Year'].unique()},
-                step=None
+            html.Div([
+                html.P('Visualisation type:',
+                       id='visualisation-type-text'),
+                dcc.RadioItems(
+                    id='visualisation-type-radio-items',
+                    options=[
+                        {'label': 'Aggregate', 'value': 'agg'},
+                        {'label': 'Individual', 'value': 'ind'}
+                    ],
+                    value='agg',
+                )
+            ],
+            style={'display':'inline-block','width': '48%'},
+            id='visualisation-type-container'
             ),
 
-            html.P(
-                'Drag the slider to change the month:',
-                id='month-slider-text'),
-
-            dcc.Slider(
-                id='month-slider',
-                min=df_street['Month'].min(),
-                max=df_street['Month'].max(),
-                value=df_street['Month'].min(),
-                marks={str(month): str(month) for month in df_street['Month'].unique()},
-                step=None
+            html.Div([
+                html.P('Enter a UK postal code (optional, speeds up loading):',
+                       id='postal-text'),
+                dcc.Input(
+                    id='postal-input',
+                    type='text',
+                    placeholder='Postal Code'
+                ),
+                html.Button(
+                    'Enter',
+                    id='postal-enter-button',
+                    n_clicks=0
+                )
+            ],
+            style={'display':'inline-block', 'float': 'right','width': '48%'},
+            id='postal-container'
             ),
 
-        ], id='input-container'),
+            html.Div([
+                html.P(
+                    'Select the date:',
+                    id='date-slider-text'),
+                dcc.Slider(
+                    id='year-slider',
+                    min=df_street['Year'].min(),
+                    max=df_street['Year'].max(),
+                    value=df_street['Year'].min(),
+                    marks={str(year): str(year) for year in df_street['Year'].unique()},
+                    step=None
+                ),
+                dcc.Slider(
+                    id='month-slider',
+                    min=df_street['Month'].min(),
+                    max=df_street['Month'].max(),
+                    value=df_street['Month'].min(),
+                    marks={str(month): str(month) for month in df_street['Month'].unique()},
+                    step=None
+                ),
+            ],
+            style={},
+            id='date-container'
+            ),
+
+            html.Div([
+                html.P(
+                    'Filter by crime type:',
+                    id='crime-dropdown-text'
+                ),
+                dcc.Checklist(
+                    id='select-all',
+                    options=[{'label': 'Select All', 'value': 1}],
+                    value=[]
+                ),
+                dcc.Dropdown(
+                    id='crime-dropdown',
+                    options=[
+                        {'label': c, 'value': c} for c in df_street['Crime type'].unique()
+                    ],
+                    value=['Violence and sexual offences'],
+                    multi=True
+                )
+
+            ],
+            style={},
+            id='other-options-container'
+            ),
+        ],
+        id='input-container'
+        ),
 
         html.Div([
-
+            html.P('Select chart: ',
+                   id='select-chart-text'
+            ),
+            dcc.Dropdown(
+                id='chart-dropdown',
+                options=[
+                    {'label': chart, 'value': chart} for chart in chart_options
+                ],
+                value=chart_options[0]
+            ),
+            dcc.RadioItems(
+                id='chart-type-radio-items',
+                options=[
+                    {'label': 'Whole of UK', 'value': 'whole'},
+                    {'label': 'Select from map', 'value': 'select'}
+                ],
+                value='whole',
+                labelStyle={'display':'inline'}
+            ),
             dcc.Graph(
-                id='scatter-map'
+                id='selected-chart'
             )
 
-        ], id='map-container')
+        ],
+        id='graph-container'
+        ),
+
     ],
     id='left-column',
     style={'width': '48%', 'display': 'inline-block','background-color': 'powderblue'}
     ),
 
     html.Div([
-        html.Div([
+        dcc.Graph(
+            id='scatter-map',
+            style={'height': '100%'}
+        )
+    ],
+    style={'width': '48%', 'display': 'inline-block', 'float': 'right',
+           'height': '100%'},
+    id='right-column'
+    )
 
-        ]),
-    ], style={'width': '48%', 'float': 'right', 'display': 'inline-block',
-              'background-color': 'powderblue'}),
 ])
 
 @app.callback(
     Output('scatter-map', 'figure'),
     Input('year-slider', 'value'),
-    Input('month-slider', 'value')
+    Input('month-slider', 'value'),
+    Input('crime-dropdown', 'value')
 )
-def update_map(year, month):
-    dff = df_street[(df_street['Year']==year) & (df_street['Month']==month)]
+def update_map(year, month, crime_types):
+    dff = df_street[(df_street['Year']==year) & (df_street['Month']==month)
+    & (df_street['Crime type'].isin(crime_types))]
 
     fig = px.scatter_mapbox(dff,
                             lat="Latitude",
@@ -112,7 +205,8 @@ def update_map(year, month):
                             hover_name="Crime type",
                             hover_data=["Location"],
                             color="Crime type",
-                            zoom=5)
+                            zoom=5,
+                            )
     fig.update_layout(mapbox_style="dark", mapbox_accesstoken=token)
 
     return fig
